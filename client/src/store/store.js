ece5ed5a0ce74e158458f30ce5b4282818350a9f
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-01-31 01:42:41 +0300

    Remove unnecessary comments

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,33 +1,31 @@
 import { createStore } from "vuex";
 
-// Проверяем сохраненное состояние авторизации
 const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
 const JWT_TOKEN = localStorage.getItem("JWT_TOKEN");
 
 export default createStore({
     state: {
-        isAuthenticated: isAuthenticated, // Состояние авторизации
-        user: null, // Данные пользователя
-        JWT_TOKEN: localStorage.getItem("JWT_TOKEN"), // Токен из localStorage
+        isAuthenticated: isAuthenticated,
+        user: null,
+        JWT_TOKEN: localStorage.getItem("JWT_TOKEN"),
     },
     mutations: {
-        // Устанавливаем состояние авторизации и данные пользователя
         SET_AUTH(state, payload) {
             state.isAuthenticated = payload.isAuthenticated;
             state.user = payload.user;
-            localStorage.setItem("isAuthenticated", payload.isAuthenticated.toString()); // Сохраняем в localStorage
+            localStorage.setItem("isAuthenticated", payload.isAuthenticated.toString());
         },
         SET_JWT_TOKEN(state, token) {
             state.JWT_TOKEN = token;
-            localStorage.setItem("JWT_TOKEN", token); // Сохраняем токен в localStorage
-        }
+            localStorage.setItem("JWT_TOKEN", token);
+        },
     },
     actions: {
-        // Мы не изменяем actions, так как запрос и получение токена происходит в другом месте
+        // Here will be the code for actions
     },
     getters: {
-        isAuthenticated: (state) => state.isAuthenticated, // Геттер для проверки авторизации
-        user: (state) => state.user, // Геттер для получения данных пользователя
-        JWT_TOKEN: (state) => state.JWT_TOKEN, // Геттер для получения токена
+        isAuthenticated: (state) => state.isAuthenticated,
+        user: (state) => state.user,
+        JWT_TOKEN: (state) => state.JWT_TOKEN,
     },
 });
