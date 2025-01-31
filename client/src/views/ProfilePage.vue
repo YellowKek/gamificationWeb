51a8b69cb8d95eb61bacb16552b0eff7b3a92856
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-01-31 03:54:41 +0300

    Create a profile page

---
+++ client/src/views/ProfilePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="for_test"></div>
+</template>
+
+<script setup></script>
+
+<style>
+.for_test {
+    width: 100vw;
+    height: 100vh;
+
+    background-color: red;
+}
+</style>
