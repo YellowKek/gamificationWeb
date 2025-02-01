cf3a183a3e4c2191038e8e116eb28bdd021b03fc
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-02-01 13:56:06 +0300

    Import new components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,14 @@
 <template>
+    <Header v-if="!$route.meta.hideHeader" />
+    <InputForm v-if="!$route.meta.hideHeader" />
     <router-view />
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+
+import Header from "./components/layout/Header.vue";
+import InputForm from "./components/layout/InputForm.vue";
 </script>
 
 <style>
@@ -11,15 +16,15 @@ body {
     width: 100vw;
     height: 100vh;
     min-width: 320px;
+    padding: 51px 0px;
     overflow-x: hidden;
     overflow-y: scroll;
     background: #020418;
 
-    font-family: "Inter", sans-serif;
-    font-optical-sizing: auto;
+    font-family: "Gilroy", sans-serif;
     font-weight: 400;
     font-style: normal;
-    line-height: 24px;
+    line-height: normal;
     color: #000000;
 }
 </style>
