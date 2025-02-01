06f386869ff6caba184eb03d9fb88e106dd4abcd
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-02-01 13:55:36 +0300

    Create a basic page structure

--- client/src/views/ProfilePage.vue
+++ client/src/views/ProfilePage.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="for_test"></div>
+    <main class="profile_page_container">
+        <UserInfo />
+    </main>
 </template>
 
-<script setup></script>
+<script setup>
+import UserInfo from "../components/profileComponents/UserInfo.vue";
+
+
+</script>
 
 <style>
-.for_test {
+.profile_page_container {
     width: 100vw;
     height: 100vh;
+    padding: 0px 120px 0px 185px;
 
-    background-color: red;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
 }
 </style>
