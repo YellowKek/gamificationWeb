764d9327c390cfd63a3108cd187a163a419e7d0a
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-02-01 13:50:43 +0300

    Create common site components

---
+++ client/src/components/layout/Header.vue
@@ -0,0 +1,91 @@
+<template>
+    <header class="header_container">
+        <nav>
+            <div class="header_link_btn_container">
+                <router-link class="header_btn" v-for="(link, index) in header_link" :key="index" :to="link.to">{{ link.label }}</router-link>
+            </div>
+            <div class="header_buttons">
+                <button class="header_square_btn">
+                    <img class="header_btn_icon" :src="notificationIcon" alt="notification icon" />
+                </button>
+                <router-link to="/Home" class="header_square_btn">
+                    <img class="header_btn_icon" :src="profileIcon" alt="profile icon" />
+                </router-link>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import notificationIcon from "../../assets/img/svg/header-icon/notification-icon.svg";
+import profileIcon from "../../assets/img/svg/header-icon/profile-icon.svg";
+
+const header_link = [
+    {
+        to: "/Home",
+        label: "Календарь",
+    },
+    {
+        to: "/Home",
+        label: "Кланы",
+    },
+    {
+        to: "/Home",
+        label: "FAQ",
+    },
+];
+</script>
+
+<style>
+.header_container {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    nav {
+        position: relative;
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
+    }
+}
+
+.header_link_btn_container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 45px;
+}
+
+.header_btn {
+    font-size: 22px;
+    font-weight: 500;
+    color: #e9eaea;
+}
+
+.header_buttons {
+    margin-right: 185px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 18px;
+}
+
+.header_square_btn {
+    min-width: 47px;
+    min-height: 44px;
+    cursor: pointer;
+    border-radius: 10px;
+    background: #0440e4;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ client/src/components/layout/InputForm.vue
@@ -0,0 +1,74 @@
+<template>
+    <section class="input_form_wrapper">
+        <div class="input_form_container" @focus="onFocus" @blur="onBlur" tabindex="0">
+            <img class="loupe_icon" :src="loupeIcon" alt="loupe icon" />
+            <input id="id" type="text" placeholder="Искать" />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import loupeIcon from "../../assets/img/svg/loupe-icon.svg";
+
+const isFocused = ref(false);
+
+const onFocus = () => {
+    isFocused.value = true;
+    console.log("Input is focused");
+};
+
+const onBlur = () => {
+    isFocused.value = false;
+    console.log("Input lost focus");
+};
+</script>
+
+<style>
+.input_form_wrapper {
+    width: 100%;
+    margin-top: 36px;
+    margin-bottom: 60px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.input_form_container {
+    position: relative;
+    width: 633px;
+    height: 45px;
+    cursor: text;
+    border-radius: 8.65px;
+    background: #e9eaea;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 13px;
+}
+
+.loupe_icon {
+    position: absolute;
+    top: 12.5px;
+    left: 22px;
+    pointer-events: none;
+}
+
+input {
+    width: 100%;
+    height: 100%;
+    padding: 55px;
+
+    font-size: 18px;
+    font-weight: 400;
+    color: #000;
+}
+
+input::placeholder {
+    font-size: 18px;
+    font-weight: 400;
+    color: #5a5b67;
+}
+</style>
